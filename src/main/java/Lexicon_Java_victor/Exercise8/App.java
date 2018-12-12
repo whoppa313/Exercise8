21b204303b8a320497993cce258221dedbd6872f
Victor_506 <viho98001@utb.vaxjo.se>
2018-12-12 16:01:10 +0100

    Exercise8

---
+++ src/main/java/Lexicon_Java_victor/Exercise8/App.java
@@ -0,0 +1,35 @@
+package Lexicon_Java_victor.Exercise8;
+
+import java.util.Random;
+import java.util.Scanner; 
+public class App 
+{
+    public static void main( String[] args )
+    {
+      
+Random rand = new Random();
+int Nummer = rand.nextInt(500) +1;
+
+Scanner input = new Scanner(System.in);
+int gissa;
+boolean vinna = false;
+
+while (vinna == false)
+{
+	System.out.println("gissa ett nummer mellan 1 och 500");
+	gissa = input.nextInt();
+	
+	if(gissa == Nummer)
+	{
+		
+	}
+
+	
+	
+	
+	
+	
+    		
+    }
+}
+
